0242fc1901f30705ae561f44e400c8b5d1f488b2
Adam Benzidane <adam.benzidane@epitech.eu>
2025-10-26 23:22:50 +0100

    fix: ogsl user replaced by root user in django settings file

--- INF37407/INF37407/settings.py
+++ INF37407/INF37407/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ogsl',
-        'USER': 'ogsl',
+        'USER': 'root',
         'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',

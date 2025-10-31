a6d4c5d8e9d2654a97104af061aec23dd02e81a4
Adam Benzidane <adam.benzidane@epitech.eu>
2025-10-31 13:20:42 -0400

    feat: ok last script i swear

--- INF37407/build.sh
+++ INF37407/build.sh
@@ -2,11 +2,3 @@
 set -e
 
 pip install -r requirements.txt
-mysql -u root -p -e "CREATE DATABASE `INF37407` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;"
-python manage.py makemigrations
-python manage.py migrate
-python manage.py parser_arcgis
-DJANGO_SUPERUSER_USERNAME=admin \
-DJANGO_SUPERUSER_EMAIL=admin@example.com \
-DJANGO_SUPERUSER_PASSWORD=admin123 \
-python manage.py createsuperuser --noinput || true
